8deff9ffec1fdc26be71ee7f0f42b163dbdbf279
Mike Pattyn <mike.pattyn1@gmail.com>
2023-04-05 16:11:18 +0200

    Everything in root NORUN

--- Flyingdarts.Persistence/ApplicationOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Flyingdarts.Persistence;
-public class ApplicationOptions
-{
-    public string DynamoDbTable { get; set; }
-    public DynamoDBOperationConfig ToOperationConfig()
-    {
-        return new DynamoDBOperationConfig { OverrideTableName = DynamoDbTable };
-    }
-}
