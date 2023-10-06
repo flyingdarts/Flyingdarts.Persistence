b1af7a5f91d3b839b91b6b0c76305435a531a3aa
Mike Pattyn <mike.pattyn1@gmail.com>
2023-10-06 11:30:58 +0200

    Update service

--- IDynamoDbService.cs
+++ IDynamoDbService.cs
@@ -1,10 +1,14 @@
-namespace Flyingdarts.Persistence;
+﻿using Flyingdarts.Persistence;
+
 public interface IDynamoDbService
 {
-    public Task<Game> GetGame(long gameId);
-    public Task<List<GamePlayer>> GetGamePlayers(long gameId);
-    public Task<List<GameDart>> GetGamePlayerGameDarts(long gameId);
-    public Task PutGame(Game request);
-    public Task PutGamePlayer(GamePlayer gamePlayer);
-    public Task PutGameDart(GameDart gameDart);
+    Task<Game> ReadGameAsync(long gameId, CancellationToken cancellationToken);
+    Task<List<GamePlayer>> ReadGamePlayersAsync(long gameId, CancellationToken cancellationToken);
+    Task<List<GameDart>> ReadGameDartsAsync(long gameId, CancellationToken cancellationToken);
+    Task<List<User>> ReadUsersAsync(string[] userIds, CancellationToken cancellationToken);
+    Task<User> ReadUserAsync(string userId, CancellationToken cancellationToken);
+    Task WriteUserAsync(User user, CancellationToken cancellationToken);
+    Task WriteGameAsync(Game game, CancellationToken cancellationToken);
+    Task WriteGamePlayerAsync(GamePlayer player, CancellationToken cancellationToken);
+    Task WriteGameDartAsync(GameDart dart, CancellationToken cancellationToken);
 }
